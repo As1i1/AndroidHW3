6ccffcdac925a79af87fde9c5bc415082c315842
Lolly <lollysens@MacBook-Pro-Lolly.local>
2024-12-23 21:08:18 +0300

    add swagger

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import time
-from backend.database import USERS, DEBTS, DEBT_ITEMS
+from database import USERS, DEBTS, DEBT_ITEMS
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -16,16 +16,15 @@ def auth_user():
     phone_number = request.args.get('phoneNumber')
     password = request.args.get('password')
     for user in USERS:
-        if phone_number == user['phone_number'] and password == user['password']:
+        if phone_number == user['phoneNumber'] and password == user['password']:
             return jsonify(user), 200
     return jsonify({'error': 'user not found'}), 404
 
 
-@app.route('/v1/user/me/')
-def user_me():
-    user_id = request.args.get('id')
+@app.route('/v1/user/<int:id>/')
+def user_me(id_):
     for user in USERS:
-        if user_id == user['id']:
+        if id_ == user['id']:
             return jsonify(user), 200
     return jsonify({'error': 'user not found'}), 404
 
@@ -52,24 +51,24 @@ def get_debts():
     return jsonify(res), 200
 
 
-@app.route('/v1/debt/<int:id>/items/')
-def debt_items(id_):
+@app.route('/v1/debt/<int:debt_id>/items/')
+def debt_items(debt_id: int):
     items = []
     owner_id = 0
     phone_number = ""
     for debt_item in DEBT_ITEMS:
-        if debt_item["id"] != id_:
+        if debt_item["debt_id"] != debt_id:
             continue
         items.append(debt_item)
     for debt in DEBTS:
-        if debt["id"] != id_:
+        if debt["id"] != debt_id:
             continue
         owner_id = debt["owner_id"]
     for user in USERS:
         if user["id"] != owner_id:
             continue
-        phone_number = user["phone_number"]
-    return jsonify({"items": items, "owner_id": owner_id, "phone_number": phone_number}), 200
+        phone_number = user["phoneNumber"]
+    return jsonify(items), 200
 
 
 if __name__ == '__main__':
